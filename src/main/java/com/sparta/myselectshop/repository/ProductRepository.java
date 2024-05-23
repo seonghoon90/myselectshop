ab2854a01b7c661b9d0ea4585d830040ce923aa8
hoon <dotofi@naver.com>
2024-05-23 15:33:46 +0900

    관심상품 희망 최저가 업데이트 API

--- src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
+++ src/main/java/com/sparta/myselectshop/repository/ProductRepository.java
@@ -3,5 +3,5 @@ package com.sparta.myselectshop.repository;
 import com.sparta.myselectshop.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<Product, Integer> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
